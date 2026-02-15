1ab5c950fd8661239f1b18bc267a6268b56ad8fe
Anshul Tomar <tanshul620@gmail.com>
2026-02-15 15:55:25 +0530

    updated

---
+++ JavaScript/Part4/data.js
@@ -0,0 +1,9 @@
+const products = [
+    {id: 1,name: "Product1",description: "Expensive", price: 50000, category: "Laptop"},
+    {id: 2,name: "Product2",description: "Expensive", price: 25000, category: "Desktop"},
+    {id: 3,name: "Product3",description: "Expensive", price: 15000, category: "Mobile"},
+    {id: 4,name: "Product4",description: "Expensive", price: 80000, category: "Television"},
+    {id: 5,name: "Product5",description: "Expensive", price: 22000, category: "Monitor"},
+    {id: 6,name: "Product6",description: "Expensive", price: 56000, category: "PC"}
+];
+export default products
---
+++ JavaScript/Part4/file30.js
@@ -0,0 +1,8 @@
+// import {add,subtract} from "./calc.js"
+import products from "./data.js"
+// const result = add(4,5)
+// console.log(result)
+
+// const result1 = subtract(4,5)
+// console.log(result1)
+console.log(products)
